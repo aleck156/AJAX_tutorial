c1944b0d183d123d629aca1dc4319d43d4f948c1
Aleksander Jaworski <aleck156@gmail.com>
2022-04-06 10:17:39 +0200

    grab the data for Poland from restcountries.com api

--- script.js
+++ script.js
@@ -4,3 +4,13 @@ const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
 
 ///////////////////////////////////////
+const req = new XMLHttpRequest();
+req.open('GET', 'https://restcountries.com/v3.1/name/poland');
+req.send();
+
+console.log(req.responseText);
+
+req.addEventListener('load', function () {
+  const [data] = JSON.parse(this.responseText);
+  console.log(data);
+});
