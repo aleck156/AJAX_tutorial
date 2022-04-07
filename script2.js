0bedd7409a12d5f6659931c8a5f8b82a6724b43d
Aleksander Jaworski <aleck156@gmail.com>
2022-04-07 10:22:05 +0200

    learning fetch().then()

--- script2.js
+++ script2.js
@@ -2,3 +2,38 @@
 
 const btn = document.querySelector('.btn-country');
 const countriesContainer = document.querySelector('.countries');
+
+const renderCountry = function (data, className = '') {
+  const currencies = data.currencies[Object.keys(data.currencies)[0]].name;
+
+  const html = ` 
+  <article class="country ${className}">
+  <img class="country__img" src="${data.flags.png}" />
+  <div class="country__data">
+        <h3 class="country__name">${data.name.common}</h3>
+        <h4 class="country__region">${data.subregion}</h4>
+        <p class="country__row"><span>👫</span>${(
+          +data.population / 1_000_000
+        ).toFixed(2)}M people</p>
+          <p class="country__row"><span>🗣️</span>${Object.values(
+            data.languages
+          ).join(', ')}</p>
+            <p class="country__row"><span>💰</span>${currencies}</p>
+            </div>
+            </article>`;
+
+  countriesContainer.insertAdjacentHTML('beforeend', html);
+  countriesContainer.style.opacity = 1;
+};
+
+// const request = fetch(`https://restcountries.com/v3.1/name/poland`);
+
+// request.then(e => console.log(e));
+
+const getCountryData = function (country) {
+  fetch(`https://restcountries.com/v3.1/name/${country}`).then(function (res) {
+    console.log(res);
+  });
+};
+
+getCountryData('poland');
