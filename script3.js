fce9641822d3898502424cc3d11b53ddf52a1274
Aleksander Jaworski <aleck156@gmail.com>
2022-04-10 07:44:36 +0200

    promisifying callback methods

---
+++ script3.js
@@ -0,0 +1,26 @@
+'use strict';
+
+const lotteryPromise = new Promise(function (resolve, reject) {
+  console.log(`Lottery draw is happening ...`);
+  setTimeout(() => {
+    if (Math.random() >= 0.5) {
+      resolve(`you win`);
+    } else {
+      reject(new Error(`you lose`));
+    }
+  }, 2000);
+});
+
+lotteryPromise.then(res => console.log(res)).catch(e => console.error(e));
+
+// Promisifying setTimeout
+const wait = function (seconds) {
+  return new Promise((resolve, reject) => setTimeout(resolve, seconds * 1000));
+};
+
+wait(2)
+  .then(() => {
+    console.log(`success!`);
+    return wait(1);
+  })
+  .then(() => console.log(`and another 1 second has passed ...`));
