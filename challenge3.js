a97a5ad890ca359155ac26f3c296d2d382441088
Aleksander Jaworski <aleck156@gmail.com>
2022-04-11 11:12:53 +0200

    grab the elements from index page, call the loadNPause function

--- challenge3.js
+++ challenge3.js
@@ -1,5 +1,8 @@
 'use strict';
 
+const images = document.querySelector('.images');
+const newImage = document.createElement('img');
+
 const createImage = function (imgPath) {
   return new Promise(function (resolve, reject) {
     newImage.src = imgPath;
@@ -15,4 +18,23 @@ const createImage = function (imgPath) {
   });
 };
 
-const loadNPause = async function () {};
+// createImage('./img/img-1.jpg')
+//   .then(res => {
+//     return wait(2);
+//   })
+//   .then(() => {
+//     newImage.style.display = 'none';
+//     return wait(2);
+//   })
+//   .then(() => {
+//     newImage.style.display = 'flex';
+//     return createImage('./img/img-2.jpg');
+//   })
+//   .catch(err => console.error(`erorr: ${err}`));
+
+const loadNPause = async function () {
+  const res = await createImage(`./img/img-1.jpg`);
+  console.log(res);
+};
+
+loadNPause();
