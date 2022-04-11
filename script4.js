ae94fbe39b51d671e203c2a4002823128b350dd8
Aleksander Jaworski <aleck156@gmail.com>
2022-04-11 11:09:29 +0200

    completed the module, moving on to solve challenge #3

--- script4.js
+++ script4.js
@@ -10,6 +10,8 @@ const getPosition = function () {
   });
 };
 
+getPosition().then(res => console.log(res.coords));
+
 const whereAmI = async function () {
   const pos = await getPosition();
   const { latitude: lat, longitude: lng } = pos.coords;
@@ -31,8 +33,12 @@ const whereAmI = async function () {
       .then(res => console.log(res));
 
   */
+
+  return `You are in ${geocodeJSON.city}, ${geocodeJSON.country}`;
 };
-whereAmI();
+const myPlace = whereAmI();
+// console.log(myPlace);
+myPlace.then(res => console.log(res));
 
 console.log(`async/await`);
 
@@ -59,3 +65,63 @@ const renderCountry = function (data, className = '') {
   countriesContainer.insertAdjacentHTML('beforeend', html);
   countriesContainer.style.opacity = 1;
 };
+
+const get3Countries = async function (c1, c2, c3) {
+  try {
+    // const [data1] = await getJSON(`https://restcountries.com/v3.1/name/${c1}`);
+    // const [data2] = await getJSON(`https://restcountries.com/v3.1/name/${c2}`);
+    // const [data3] = await getJSON(`https://restcountries.com/v3.1/name/${c3}`);
+
+    const data = await Promise.all([
+      getJSON(`https://restcountries.com/v3.1/name/${c1}`),
+      getJSON(`https://restcountries.com/v3.1/name/${c2}`),
+      getJSON(`https://restcountries.com/v3.1/name/${c3}`),
+    ]);
+
+    console.log(data.map(d => d[0].capital));
+
+    // console.log([data1.capital, data2.capital, data3.capital]);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const getJSON = function (url, errorMsg = 'Something went Horribly wrong') {
+  return fetch(url).then(res => {
+    if (!res.ok) throw new Error(`${errorMsg} (${res.status})`);
+    return res.json();
+  });
+};
+
+get3Countries('poland', 'france', 'italy');
+
+// promise.race
+// the first promise settled wins the race
+// this includes the rejected ones
+(async function () {
+  const res = await Promise.race([
+    getJSON(`https://restcountries.com/v3.1/name/italy`),
+    getJSON(`https://restcountries.com/v3.1/name/spain`),
+    getJSON(`https://restcountries.com/v3.1/name/portugal`),
+  ]);
+  console.log([res][0]);
+})();
+
+const timeout = function (seconds) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(new Error(`request got rejected`));
+    }, seconds * 1000);
+  });
+};
+
+Promise.race([
+  getJSON(`https://restcountries.com/v3.1/name/tanzania`),
+  timeout(0.1),
+])
+  .then(res => console.log(res[0]))
+  .catch(err => console.error(err));
+
+// promise .allSettled
+// only once all promises are settled
+// .all() short-circuits when one is rejected
