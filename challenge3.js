5456823bc7d059f4e2e61a3040c02865d275207a
Aleksander Jaworski <aleck156@gmail.com>
2022-04-11 11:10:50 +0200

    reuse createImage code

--- challenge3.js
+++ challenge3.js
@@ -1,1 +1,18 @@
 'use strict';
+
+const createImage = function (imgPath) {
+  return new Promise(function (resolve, reject) {
+    newImage.src = imgPath;
+
+    newImage.addEventListener('error', function () {
+      reject(new Error(`Image not found: ${imgPath}`));
+    });
+    newImage.addEventListener('load', () => {
+      images.insertAdjacentElement('afterbegin', newImage);
+    });
+
+    return resolve(newImage);
+  });
+};
+
+const loadNPause = async function () {};
